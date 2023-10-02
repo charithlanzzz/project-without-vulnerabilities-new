d86c1a97e0d82c89be3423943ecc09001725159a
charithlanzzz <lansakaracharith@gmail.com>
2023-10-02 15:44:23 +0530

    changes

--- src/main/java/com/scalesec/vulnado/Postgres.java
+++ src/main/java/com/scalesec/vulnado/Postgres.java
@@ -2,7 +2,6 @@ package com.scalesec.vulnado;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.PreparedStatement;
@@ -23,20 +22,21 @@ public class Postgres {
                     System.getenv("PGUSER"), System.getenv("PGPASSWORD"));
         } catch (Exception e) {
             e.printStackTrace();
-            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(1);
         }
         return null;
     }
-    public static void setup(){
+
+    public static void setup() {
         try {
             System.out.println("Setting up Database...");
             Connection c = connection();
             Statement stmt = c.createStatement();
 
             // Create Schema
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users(user_id VARCHAR (36) PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (50) NOT NULL, created_on TIMESTAMP NOT NULL, last_login TIMESTAMP)");
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS comments(id VARCHAR (36) PRIMARY KEY, username VARCHAR (36), body VARCHAR (500), created_on TIMESTAMP NOT NULL)");
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users(user_id UUID PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (64) NOT NULL, created_on TIMESTAMP NOT NULL, last_login TIMESTAMP)");
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS comments(id UUID PRIMARY KEY, username UUID, body VARCHAR (500), created_on TIMESTAMP NOT NULL)");
 
             // Clean up any existing data
             stmt.executeUpdate("DELETE FROM users");
@@ -58,59 +58,43 @@ public class Postgres {
         }
     }
 
-    // Java program to calculate MD5 hash value
-    public static String md5(String input)
-    {
+    // Java program to calculate SHA-256 hash value
+    public static String sha256(String input) {
         try {
-
-            // Static getInstance method is called with hashing MD5
-            MessageDigest md = MessageDigest.getInstance("MD5");
-
-            // digest() method is called to calculate message digest
-            //  of an input digest() return array of byte
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
             byte[] messageDigest = md.digest(input.getBytes());
-
-            // Convert byte array into signum representation
-            BigInteger no = new BigInteger(1, messageDigest);
-
-            // Convert message digest into hex value
-            String hashtext = no.toString(16);
-            while (hashtext.length() < 32) {
-                hashtext = "0" + hashtext;
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : messageDigest) {
+                hexString.append(String.format("%02x", b));
             }
-            return hashtext;
-        }
-
-        // For specifying wrong message digest algorithms
-        catch (NoSuchAlgorithmException e) {
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
 
     private static void insertUser(String username, String password) {
-       String sql = "INSERT INTO users (user_id, username, password, created_on) VALUES (?, ?, ?, current_timestamp)";
-       PreparedStatement pStatement = null;
-       try {
-          pStatement = connection().prepareStatement(sql);
-          pStatement.setString(1, UUID.randomUUID().toString());
-          pStatement.setString(2, username);
-          pStatement.setString(3, md5(password));
-          pStatement.executeUpdate();
-       } catch(Exception e) {
-         e.printStackTrace();
-       }
+        String sql = "INSERT INTO users (user_id, username, password, created_on) VALUES (?, ?, ?, current_timestamp)";
+        try (Connection connection = connection();
+             PreparedStatement pStatement = connection.prepareStatement(sql)) {
+            pStatement.setObject(1, UUID.randomUUID());
+            pStatement.setString(2, username);
+            pStatement.setString(3, sha256(password));
+            pStatement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static void insertComment(String username, String body) {
         String sql = "INSERT INTO comments (id, username, body, created_on) VALUES (?, ?, ?, current_timestamp)";
-        PreparedStatement pStatement = null;
-        try {
-            pStatement = connection().prepareStatement(sql);
-            pStatement.setString(1, UUID.randomUUID().toString());
-            pStatement.setString(2, username);
+        try (Connection connection = connection();
+             PreparedStatement pStatement = connection.prepareStatement(sql)) {
+            pStatement.setObject(1, UUID.randomUUID());
+            pStatement.setObject(2, UUID.fromString(username)); // Assuming username is UUID
             pStatement.setString(3, body);
             pStatement.executeUpdate();
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
