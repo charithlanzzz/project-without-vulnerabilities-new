fef5021295b689e6a88d9026f3e59f1b2ff303d2
charithlanzzz <lansakaracharith@gmail.com>
2023-10-02 19:18:36 +0530

    changes

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -1,5 +1,7 @@
 package com.scalesec.vulnado;
 
+import com.scalesec.vulnado.CustomBadRequestException;
+import com.scalesec.vulnado.LinkLister;
 import org.springframework.boot.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -7,17 +9,42 @@ import org.springframework.boot.autoconfigure.*;
 import java.util.List;
 import java.io.Serializable;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @RestController
 @EnableAutoConfiguration
 public class LinksController {
+  private static final Set<String> ALLOWED_DOMAINS = new HashSet<>(Arrays.asList("example.com", "trusted-domain.com"));
+
   @RequestMapping(value = "/links", produces = "application/json")
-  List<String> links(@RequestParam String url) throws IOException{
-    return LinkLister.getLinks(url);
+  List<String> links(@RequestParam String url) throws IOException {
+    if (isValidUrl(url)) {
+      return LinkLister.getLinks(url);
+    } else {
+      throw new CustomBadRequestException("Invalid URL");
+    }
   }
+
   @RequestMapping(value = "/links-v2", produces = "application/json")
-  List<String> linksV2(@RequestParam String url) throws BadRequest{
-    return LinkLister.getLinksV2(url);
+  List<String> linksV2(@RequestParam String url) throws CustomBadRequestException, IOException {
+    if (isValidUrl(url)) {
+      return LinkLister.getLinksV2(url);
+    } else {
+      throw new CustomBadRequestException("Invalid URL");
+    }
+  }
+
+  private boolean isValidUrl(String url) {
+    try {
+      URL inputUrl = new URL(url);
+      String host = inputUrl.getHost();
+      return ALLOWED_DOMAINS.contains(host);
+    } catch (Exception e) {
+      return false;
+    }
   }
 }
