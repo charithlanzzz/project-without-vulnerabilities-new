a1b551c598fd701055145c3d6de7840ef68469fe
charithlanzzz <lansakaracharith@gmail.com>
2023-10-02 13:42:29 +0530

    changes

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -3,13 +3,42 @@ package com.scalesec.vulnado;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
 
-import java.io.Serializable;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @RestController
 @EnableAutoConfiguration
 public class CowController {
-    @RequestMapping(value = "/cowsay")
-    String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
-        return Cowsay.run(input);
+    @RequestMapping(value = "/cowsay", method = RequestMethod.GET)
+    String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) throws IOException {
+        // Sanitize the input to prevent command injection
+        input = input.replaceAll("[^a-zA-Z0-9]", ""); // Remove any characters that are not alphanumeric
+
+        // Build and execute the cowsay command using ProcessBuilder
+        ProcessBuilder processBuilder = new ProcessBuilder("cowsay", input);
+
+        // Redirect error stream to output stream
+        processBuilder.redirectErrorStream(true);
+
+        Process process = processBuilder.start();
+
+        // Read the command's output
+        StringBuilder output = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+        }
+
+        // Wait for the process to finish
+        try {
+            process.waitFor();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        return output.toString();
     }
 }
